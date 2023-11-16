141295074cee75a4f41037e00772a1c7999c8642
Daniel Teles da Silva <danielltteles@gmail.com>
2023-11-16 10:14:34 -0300

    Add files via upload

---
+++ Marketplace/src/main/java/br/com/fatec/DAO/DAO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.fatec.DAO;
+import java.sql.SQLException;
+import java.util.Collection;
+
+/**
+ *
+ * @author danie
+ */
+public interface DAO <T> {
+    public boolean insere(T model) 
+                    throws SQLException;
+    public boolean remove(T model) 
+                    throws SQLException;
+    public boolean altera(T model) 
+                    throws SQLException;
+    public T buscaID(T model) 
+                    throws SQLException;
+    public Collection <T> lista(String criterio) 
+                    throws SQLException;
+    
+}
